f539238ed50afa132798f2aaf06a18beb1f315c9
DineshMitta <dineshgopimitta@gmail.com>
2024-09-22 20:46:14 +0530

    added forloop range function start end & increment with inpurts program

---
+++ for_loop_range_example.py
@@ -0,0 +1,5 @@
+start = int(input("enter the starting number : "))
+end = int(input("Enter the ending range : "))
+increment = int(input("Enter the increment value: "))
+for i in range(start, end, increment):
+    print(i)
